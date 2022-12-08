0244c43fdda7b9459567dd598868c25ba19626db
lupo <lupo.f@outlook.com>
2022-12-08 15:44:35 +0800

    精简

--- base_analysis.py
+++ base_analysis.py
@@ -1,8 +1,6 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession, DataFrame
 
-from constants import Config
-
 
 def __spark_instance__(app_name='defaultAppName'):
     spark_conf = SparkConf() \
@@ -15,10 +13,6 @@ def __spark_instance__(app_name='defaultAppName'):
 
 
 class Analysis:
-    """
-
-    """
-
     def __init__(self, app_name='defaultAppName'):
         self.__sc__, self.__spark__ = __spark_instance__(app_name)
 
@@ -32,30 +26,8 @@ class Analysis:
     def __run__(self):
         pass
 
-    def __save_to_db__(self, df: DataFrame, table, mode, jdbc: Config = None):
-        """
-        :param jdbc:
-        :param df:
-        :param table:
-        :param mode:
-            * `append`: Append contents of this :class:`DataFrame` to existing data.
-            * `overwrite`: Overwrite existing data.
-            * `error` or `errorifexists`: Throw an exception if data already exists.
-            * `ignore`: Silently ignore this operation if data already exists.
-        :return:
-        """
-        print('target table: {}'.format(table))
-        if jdbc is None:
-            raise Exception("jdbc is none.")
-        df.write.jdbc(url=jdbc.url(), table=table, mode=mode, properties=jdbc.properties())
-
-    def __read_from_db__(self, table, jdbc: Config = None) -> DataFrame:
-        if jdbc is None:
-            raise Exception("jdbc is none.")
-        return self.__spark__.read.jdbc(url=jdbc.url(), table=table, properties=jdbc.properties())
-
     def __read_from_csv__(self, path) -> DataFrame:
-        return self.__spark__.read.csv(path)
+        return self.__spark__.read.csv(path, header=True)
 
     def __save_to_csv__(self, df: DataFrame, path, mode='overwrite'):
         print('target csv: {}'.format(path))
--- demo.py
+++ demo.py
@@ -1,15 +1,13 @@
 from base_analysis import Analysis
-from constants import Config
-
-DEV_CONSTANT = Config("db_type", "ip", "port", "db", "user", "passwd")
 
 
 class Demo(Analysis):
 
     def __run__(self):
         # self.__spark__.sql('select 1 from dual').createOrReplaceTempView('data')
-        pass
+        print('>>>>> start.')
+        print('>>>>> finish.')
 
 
 if __name__ == '__main__':
-    Demo(DEV_CONSTANT).start()
+    Demo().start()
